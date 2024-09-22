e74e4fc9be5dba8dee76b7de7b212d3dd655ef35
Alexia <trup.lexa@gmail.com>
2024-09-22 20:14:23 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 23547;
+        int miles = ticketPrice / 20;
+
+        System.out.println("Начисленно бонусных миль: " + miles);
+
+    }
+}
